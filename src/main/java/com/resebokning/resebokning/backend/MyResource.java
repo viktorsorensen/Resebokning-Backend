1dbcd7deedaeb0ad129a52d2d7a4dd18dd00c4dc
viktorsorensen <viktor.sorensen87@outlook.com>
2016-04-18 20:14:24 +0200

    Bla

--- src/main/java/com/resebokning/resebokning/backend/MyResource.java
+++ src/main/java/com/resebokning/resebokning/backend/MyResource.java
@@ -8,15 +8,9 @@ import javax.ws.rs.core.MediaType;
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("info")
 public class MyResource {
 
-    /**
-     * Method handling HTTaaP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
